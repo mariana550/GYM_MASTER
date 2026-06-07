20c6f8f5e05ebe2982bdcc5204c8e0e23796512c
mariana villegas <mariana550@gmail.com>
2026-06-07 18:49:03 -0500

    ejecuta migraciones

--- backend/Program.cs
+++ backend/Program.cs
@@ -117,4 +117,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// Ejecutar migraciones automáticamente
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
